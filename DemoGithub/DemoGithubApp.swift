1c99970923a52b472fb1e805e30a3774866e1b77
DemirDorukhan <demirdorukhan@icloud.com>
2023-05-27 17:56:32 +0300

    Initial Commit

---
+++ DemoGithub/DemoGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubApp.swift
+//  DemoGithub
+//
+//  Created by Dorukhan Demir on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

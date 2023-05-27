e829c2c9885f4d6224b2934688959d8a219079b9
DemirDorukhan <demirdorukhan@icloud.com>
2023-05-27 18:36:39 +0300

    Add third view.

---
+++ DemoGithub/ThirdView.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdView.swift
+//  DemoGithub
+//
+//  Created by Dorukhan Demir on 27.05.2023.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text("This is third view.")
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
